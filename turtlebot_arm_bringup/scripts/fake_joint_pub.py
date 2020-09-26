f0518aab18e9b6c7feb5a39bc181eefff2c189d8
corot <jorge.santos@rapyuta-robotics.com>
2020-09-26 21:31:41 +0900

    Fix box_inertia macro and use it everywhere

--- turtlebot_arm_bringup/scripts/fake_joint_pub.py
+++ turtlebot_arm_bringup/scripts/fake_joint_pub.py
@@ -7,12 +7,15 @@ rospy.init_node("fake_joint_pub")
 p = rospy.Publisher('joint_states', JointState, queue_size=5)
 
 msg = JointState()
-msg.name = ["gripper_link_joint"]
+msg.name = ["gripper_link_joint", "gripper_static_joint"]
 msg.position = [0.0 for name in msg.name]
 msg.velocity = [0.0 for name in msg.name]
 
+rate = rospy.Rate(50)
 while not rospy.is_shutdown():
     msg.header.stamp = rospy.Time.now()
     p.publish(msg)
-    rospy.sleep(0.05)
-
+    try:
+        rate.sleep()
+    except rospy.exceptions.ROSInterruptException:
+        pass
